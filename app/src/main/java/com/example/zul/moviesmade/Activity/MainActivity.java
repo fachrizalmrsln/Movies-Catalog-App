dfd9320a780ec5a3d61b672f2e46437255b89df6
zul <fahrizalmursalin06@gmai.com>
2019-01-11 21:34:31 +0700

    submission 1 done

--- app/src/main/java/com/example/zul/moviesmade/Activity/MainActivity.java
+++ app/src/main/java/com/example/zul/moviesmade/Activity/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.zul.moviesmade.Activity;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
